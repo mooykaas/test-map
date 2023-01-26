f89ca7da3b2cb69ca635f418e87bf88751a7d508
marielle ooykaas <mooykaas@gmail.com>
2023-01-26 11:38:59 +0100

    nieuwe models

--- testmap/my_tennis_club/members/models.py
+++ testmap/my_tennis_club/members/models.py
@@ -1,6 +1,20 @@
-from django.db import models
+from django.db import migrations, models
 
-# Create your models here.
-class Member(models.Model):
- firstname = models.CharField(max_length=255)
- lastname = models.CharField(max_length=255)
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Member',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('firstname', models.CharField(max_length=255)),
+                ('lastname', models.CharField(max_length=255)),
+            ],
+        ),
+    ]
